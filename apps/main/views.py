ef38fdc4837251fe2017f9a244895801b44f6993
Kairat Smakov <ksmakov@gmail.com>
2023-10-09 12:57:45 +0600

    Added last action in ItemViewSet.

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Category, Item, Info
 from .serializers import (
@@ -25,6 +27,13 @@ class ItemViewSet(ReadOnlyModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     permission_classes = (AllowAny, )
+    
+    @action(detail=False)
+    def last(self, request):
+        limit = request.query_params.get('limit', '10')
+        items = Item.objects.all()[:int(limit)]
+        return Response(self.serializer_class(items, many=True).data)
+    
 
 class InfoViewSet(ReadOnlyModelViewSet):
     """
